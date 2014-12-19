db59575b8e61df9bcf2183a10c5aae679c913699
Balint Erdi <balint.erdi@gmail.com>
2014-12-19 21:35:02 +0100

    Controller test for band/songs passes

--- app/router.js
+++ app/router.js
@@ -6,6 +6,11 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.resource('bands', function() {
+    this.resource('band', function() {
+      this.route('songs');
+    });
+  });
 });
 
 export default Router;
